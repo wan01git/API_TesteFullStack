b2c29963d7edb5450513a59e02d6fdfe41e158d6
Wandeson <wanderson.gustavo@structor.com.br>
2024-02-02 23:21:57 -0300

    api

---
+++ prisma/migrations/20240203015640_init/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE `users` (
+    `id` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(191) NOT NULL,
+    `email` VARCHAR(191) NOT NULL,
+    `telefone` VARCHAR(191) NULL,
+    `estado` VARCHAR(191) NULL,
+    `cidade` VARCHAR(191) NULL,
+    `mensagem` VARCHAR(191) NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
---
+++ src/users/dto/create-user.dto.ts
@@ -0,0 +1,8 @@
+export class CreateUserDto {
+  id?: string;
+  name: string;
+  surname: string;
+  email: string;
+  phone: string;
+  password: string;
+}
